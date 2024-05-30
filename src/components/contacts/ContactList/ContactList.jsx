054e6380552d2079fb92426fa4ebef7663fea8a4
unknown <sharookh9544351042@gmail.com>
2024-05-30 23:50:14 +0530

    last

--- src/components/contacts/ContactList/ContactList.jsx
+++ src/components/contacts/ContactList/ContactList.jsx
@@ -93,7 +93,7 @@ let ContactList = () => {
                   
                     
 <button class="newcontact ms-3 mt-2" type="button">
-  <span class="nbutton__text">Add Item</span>
+  <span class="nbutton__text">New</span>
   <span class="nbutton__icon"><svg class="svg" fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><line x1="12" x2="12" y1="5" y2="19"></line><line x1="5" x2="19" y1="12" y2="12"></line></svg></span>
 </button>
                   
